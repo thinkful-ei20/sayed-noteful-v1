3dcd093b93f56e2e3a7480094c99b3d8264139b0
Sayed Khan <arsalon47@gmail.com>
2018-04-18 15:03:59 -0400

    post functionality done

---
+++ middleware/logger.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const logger = (req, res, next) => {
+  const now = new Date();
+  console.log(`${now.toLocaleDateString()} ${now.toLocaleTimeString()} ${req.method} ${req.url}`);
+  next();
+};
+
+module.exports = {logger};
--- server.js
+++ server.js
@@ -1,32 +1,44 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan');
 
-const data = require('./db/notes');
+// const data = require('./db/notes');
+// const simDB = require('./db/simDB');
+// const notes = simDB.initialize(data);
+const notesRouter = require('./router/notes.router')
+
+const {PORT} = require('./config');
+
+// const {logger} = require('./middleware/logger');
 
 const app = express();
+app.use(morgan('common'));
+
 app.use(express.static('public'));
 
-app.listen(8080, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
+app.use(express.json());
 
 console.log('Hello Noteful!');
 
-// INSERT EXPRESS APP CODE HERE...
-app.get('/api/notes', (req, res) => {
-  const query = req.query.searchTerm;
-  const filtered = data.filter(item => {
-    return item.title.includes(query) ||
-    item.content.includes(query) || !query;
-  });
-  res.json(filtered);
+app.use('/api', notesRouter);
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
 });
 
-app.get('/api/notes/:id', (req, res) => {
-  const id = req.params.id;
-  const filtered = data.find(item => item.id === Number(id));
-  res.json(filtered);
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+app.listen(PORT, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
 });
